afafa4a6e64fe457727c2ed2c158a95ea2343dd7
Marlen Brunner <klondikemarlen@gmail.com>
2025-04-23 16:45:14 -0700

    :abc: Alphabetizing models exports.
    Why? To make it easier to find stuff and avoid duplicates.

--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -1,53 +1,53 @@
 import db from "@/db/db-client"
 
 // Models
-import User from "@/models/user"
-import UserPermission from "@/models/user-permission"
-import Source from "@/models/source"
-import Retention from "@/models/retention"
-import Category from "@/models/category"
 import ArchiveItem from "@/models/archive-item"
-import ArchiveItemAudit from "./archive-item-audit"
+import ArchiveItemAudit from "@/models/archive-item-audit"
 import ArchiveItemCategory from "@/models/archive-item-category"
 import ArchiveItemFile from "@/models/archive-item-file"
+import Category from "@/models/category"
+import Retention from "@/models/retention"
+import Source from "@/models/source"
 import SourceCategory from "@/models/source-category"
+import User from "@/models/user"
+import UserPermission from "@/models/user-permission"
 
 db.addModels([
-  User,
-  UserPermission,
   ArchiveItem,
-  Source,
-  Retention,
-  Category,
   ArchiveItemAudit,
   ArchiveItemCategory,
   ArchiveItemFile,
+  Category,
+  Retention,
+  Source,
   SourceCategory,
+  User,
+  UserPermission,
 ])
 
 // Lazy load scopes
+ArchiveItem.establishScopes()
+ArchiveItemAudit.establishScopes()
+ArchiveItemCategory.establishScopes()
+ArchiveItemFile.establishScopes()
+Category.establishScopes()
+Retention.establishScopes()
+Source.establishScopes()
+SourceCategory.establishScopes()
 User.establishScopes()
 UserPermission.establishScopes()
-ArchiveItem.establishScopes()
-Source.establishScopes()
-Retention.establishScopes()
-Category.establishScopes()
-ArchiveItemCategory.establishScopes()
-ArchiveItemAudit.establishScopes()
-ArchiveItemFile.establishScopes()
-SourceCategory.establishScopes()
 
 export {
+  ArchiveItem,
+  ArchiveItemAudit,
+  ArchiveItemCategory,
+  ArchiveItemFile,
+  Category,
+  Retention,
+  Source,
+  SourceCategory,
   User,
   UserPermission,
-  Source,
-  ArchiveItem,
-  Retention,
-  Category,
-  ArchiveItemCategory,
-  ArchiveItemAudit,
-  ArchiveItemFile,
-  SourceCategory,
 }
 
 // Special db instance will all models loaded
