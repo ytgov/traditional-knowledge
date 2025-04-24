a89311d6589b1da6a71fa475085392436983048f
Marlen Brunner <klondikemarlen@gmail.com>
2025-04-24 16:51:11 -0700

    :bug: Switch to explicit exports in services/index.ts
    Why? Avoid export * unless bundling as this creates the potential for name conflicts.
    i.e.
    
    export * as Xyz from "./Xyz" is safe.
    export * from "./Xyz" is not.

--- api/src/services/index.ts
+++ api/src/services/index.ts
@@ -1,5 +1,6 @@
+export { FileStorageService } from "./file-storage-service"
+
 export * as Categories from "./categories"
 export * as Retentions from "./retentions"
 export * as Sources from "./sources"
 export * as Users from "./users"
-export * from "./file-storage-service"
