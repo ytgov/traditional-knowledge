5f68ce248faf49a7b5680c497cf167d85e87217d
Marlen Brunner <klondikemarlen@gmail.com>
2025-04-24 16:50:39 -0700

    :abc: Alphabetize exports in services/index.ts

--- api/src/services/index.ts
+++ api/src/services/index.ts
@@ -1,5 +1,5 @@
-export * as Users from "./users"
-export * as Sources from "./sources"
-export * as Retentions from "./retentions"
 export * as Categories from "./categories"
+export * as Retentions from "./retentions"
+export * as Sources from "./sources"
+export * as Users from "./users"
 export * from "./file-storage-service"
