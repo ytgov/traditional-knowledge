a8a84d780299c6bd081f0fbc08fb3f9813cbf669
Marlen Brunner <klondikemarlen@gmail.com>
2025-04-25 08:58:07 -0700

    :bug: Fix vuetify sort by to safe route query composable.
    Now uses simplified json transformer which produces uglier route query values, but is much less complex

--- web/src/use/utils/use-vuetify-sort-by-to-safe-route-query.ts
+++ web/src/use/utils/use-vuetify-sort-by-to-safe-route-query.ts
@@ -1,49 +1,19 @@
-import { isEmpty, isNil, isString } from "lodash"
 import { Ref } from "vue"
 import { useRouteQuery } from "@vueuse/router"
 
 import { type VDataTable } from "vuetify/lib/components/VDataTable/index.mjs"
 
-export type SortItem = VDataTable["sortBy"][0]
+import { jsonTransformer } from "@/utils/use-route-query-transformers"
 
-/**
- * Must not conflict with web/src/use/utils/use-vuetify-sort-by-to-sequelize-safe-order.ts SEPARATOR.
- */
-const SEPARATOR = "_"
+export type SortItem = VDataTable["sortBy"][0]
 
 export function useVuetifySortByToSafeRouteQuery(
   name: string,
   defaultValue?: SortItem[] | undefined
 ): Ref<SortItem[] | undefined> {
-  function parse(newSortBy: string[] | string | undefined): SortItem[] | undefined {
-    if (isNil(newSortBy) || isEmpty(newSortBy)) {
-      return
-    }
-
-    if (isString(newSortBy)) {
-      newSortBy = [newSortBy]
-    }
-
-    return newSortBy.map((entry) => {
-      const [key, order] = entry.split(SEPARATOR)
-      return { key, order } as SortItem
-    })
-  }
-
-  function stringify(sortByValue: SortItem[] | undefined): string[] | undefined {
-    if (isNil(sortByValue) || isEmpty(sortByValue)) {
-      return
-    }
-
-    return sortByValue.map(({ key, order }) => `${key}${SEPARATOR}${order}`)
-  }
-
-  const defaultValueString = stringify(defaultValue)
-  return useRouteQuery<string[] | undefined, SortItem[] | undefined>(name, defaultValueString, {
-    transform: {
-      get: parse,
-      set: stringify,
-    },
+  const defaultValueString = jsonTransformer.set(defaultValue)
+  return useRouteQuery<string | undefined, SortItem[] | undefined>(name, defaultValueString, {
+    transform: jsonTransformer,
   })
 }
 
---
+++ web/src/utils/use-route-query-transformers/index.ts
@@ -0,0 +1,1 @@
+export { jsonTransformer } from "./json-transformer"
