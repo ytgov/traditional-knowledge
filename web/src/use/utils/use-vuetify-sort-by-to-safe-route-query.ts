203864e5d4053579b6d6f42b0be60c62cc6d9f0f
Marlen Brunner <klondikemarlen@gmail.com>
2025-04-25 13:13:25 -0700

    :japanese_castle: Make vuetify sort by to safe route query better handle empty values.
    Prior to this clicking the default sort 3 times would return sort=[] in the url.
    Instead the url should not show anything.

--- web/src/use/utils/use-vuetify-sort-by-to-safe-route-query.ts
+++ web/src/use/utils/use-vuetify-sort-by-to-safe-route-query.ts
@@ -1,4 +1,5 @@
-import { Ref } from "vue"
+import { isEmpty } from "lodash"
+import { type Ref } from "vue"
 import { useRouteQuery } from "@vueuse/router"
 
 import { type VDataTable } from "vuetify/lib/components/VDataTable/index.mjs"
@@ -12,8 +13,24 @@ export function useVuetifySortByToSafeRouteQuery(
   defaultValue?: SortItem[] | undefined
 ): Ref<SortItem[] | undefined> {
   const defaultValueString = jsonTransformer.set(defaultValue)
+
+  function get<T>(value: string | undefined): T | undefined {
+    if (isEmpty(value)) return undefined
+
+    return jsonTransformer.get<T>(value)
+  }
+
+  function set<T>(value: T | undefined): string | undefined {
+    if (isEmpty(value)) return undefined
+
+    return jsonTransformer.set<T>(value)
+  }
+
   return useRouteQuery<string | undefined, SortItem[] | undefined>(name, defaultValueString, {
-    transform: jsonTransformer,
+    transform: {
+      get,
+      set,
+    },
   })
 }
 
