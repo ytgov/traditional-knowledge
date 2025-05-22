e33f2f5d0e25a79b1164b41c02304eca3800d61f
Marlen Brunner <klondikemarlen@gmail.com>
2025-05-22 13:26:11 -0700

    :japanese_castle: Add some extra validations to the information sharing agreement access grant creation service.
    Why? To make sure that the data is consistent, despite what the UI might or might not allow.
    In the future we may want to allow adding arbitrary users to an information sharing agreement, but for now
    I'm going with the most restrictive approach.

--- api/src/services/information-sharing-agreement-access-grants/create-service.ts
+++ api/src/services/information-sharing-agreement-access-grants/create-service.ts
@@ -1,13 +1,19 @@
 import { Attributes } from "@sequelize/core"
 import { isNil } from "lodash"
 
-import { User, InformationSharingAgreementAccessGrant } from "@/models"
+import {
+  User,
+  InformationSharingAgreementAccessGrant,
+  InformationSharingAgreement,
+  UserGroup,
+} from "@/models"
 import BaseService from "@/services/base-service"
 
 export type InformationSharingAgreementAccessGrantCreationAttributes = Partial<
   Attributes<InformationSharingAgreementAccessGrant>
 >
 
+// TODO: consider nesting access grant creation under the /information-sharing-agreements namespace?
 export class CreateService extends BaseService {
   constructor(
     private attributes: InformationSharingAgreementAccessGrantCreationAttributes,
@@ -27,6 +33,26 @@ export class CreateService extends BaseService {
       throw new Error("Group id is required")
     }
 
+    // TODO: Consider if these should be model level validations?
+    const informationSharingAgreement = await this.loadInformationSharingAgreement(
+      informationSharingAgreementId
+    )
+    const { sharingGroupId, receivingGroupId } = informationSharingAgreement
+    await this.assertGroupIdIsValidInformationSharingAgreementGroup(
+      groupId,
+      sharingGroupId,
+      receivingGroupId
+    )
+
+    const { userId } = this.attributes
+    if (!isNil(userId)) {
+      await this.assertUserIdIsValidInformationSharingAgreementGroupMember(
+        userId,
+        sharingGroupId,
+        receivingGroupId
+      )
+    }
+
     const informationSharingAgreementAccessGrant =
       await InformationSharingAgreementAccessGrant.create({
         ...optionalAttributes,
@@ -36,6 +62,48 @@ export class CreateService extends BaseService {
       })
 
     return informationSharingAgreementAccessGrant
+  }
+
+  private async assertGroupIdIsValidInformationSharingAgreementGroup(
+    groupId: number,
+    sharingGroupId: number,
+    receivingGroupId: number
+  ): Promise<void> {
+    if (groupId !== sharingGroupId && groupId !== receivingGroupId) {
+      throw new Error(
+        "Group id must match either the information sharing agreement's sharing group id or receiving group id"
+      )
+    }
+  }
+
+  private async assertUserIdIsValidInformationSharingAgreementGroupMember(
+    userId: number,
+    sharingGroupId: number,
+    receivingGroupId: number
+  ): Promise<void> {
+    const userGroupCount = await UserGroup.count({
+      where: {
+        userId,
+        groupId: [sharingGroupId, receivingGroupId],
+      },
+    })
+    if (userGroupCount === 0) {
+      throw new Error(
+        "User must be a member of either the information sharing agreement's sharing group or receiving group"
+      )
+    }
+  }
+
+  private async loadInformationSharingAgreement(
+    informationSharingAgreementId: number
+  ): Promise<InformationSharingAgreement> {
+    const informationSharingAgreement = await InformationSharingAgreement.findByPk(
+      informationSharingAgreementId,
+      {
+        rejectOnEmpty: true,
+      }
+    )
+    return informationSharingAgreement
   }
 }
 
