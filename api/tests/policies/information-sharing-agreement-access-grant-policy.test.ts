3c995649da7b327e3f3e3b86dbe4872a4ea7e5a4
Marlen Brunner <klondikemarlen@gmail.com>
2025-05-22 10:27:02 -0700

    :white_check_mark: Add tests for information sharing agreement access grant policy "show".

--- api/tests/policies/information-sharing-agreement-access-grant-policy.test.ts
+++ api/tests/policies/information-sharing-agreement-access-grant-policy.test.ts
@@ -8,6 +8,61 @@ import {
 
 describe("api/src/policies/information-sharing-agreement-access-grant-policy.ts", () => {
   describe("InformationSharingAgreementAccessGrantPolicy", () => {
+    describe("#show", () => {
+      test("when the user has any level of access to the information sharing agreement, returns true", async () => {
+        // Arrange
+        const user = await userFactory.create()
+
+        const informationSharingAgreement = await informationSharingAgreementFactory.create()
+
+        const informationSharingAgreementAccessGrant =
+          await informationSharingAgreementAccessGrantFactory.create({
+            informationSharingAgreementId: informationSharingAgreement.id,
+            userId: user.id,
+            accessLevel: InformationSharingAgreementAccessGrant.AccessLevels.READ,
+          })
+
+        await informationSharingAgreementAccessGrant.reload({
+          include: ["siblings"],
+        })
+
+        // Act
+        const policy = new InformationSharingAgreementAccessGrantPolicy(
+          user,
+          informationSharingAgreementAccessGrant
+        )
+
+        // Assert
+        expect(policy.show()).toBe(true)
+      })
+
+      test("when the user does not have any level of access to the information sharing agreement, returns false", async () => {
+        // Arrange
+        const user = await userFactory.create()
+
+        const informationSharingAgreement = await informationSharingAgreementFactory.create()
+
+        const informationSharingAgreementAccessGrant =
+          await informationSharingAgreementAccessGrantFactory.create({
+            informationSharingAgreementId: informationSharingAgreement.id,
+            accessLevel: InformationSharingAgreementAccessGrant.AccessLevels.READ,
+          })
+
+        await informationSharingAgreementAccessGrant.reload({
+          include: ["siblings"],
+        })
+
+        // Act
+        const policy = new InformationSharingAgreementAccessGrantPolicy(
+          user,
+          informationSharingAgreementAccessGrant
+        )
+
+        // Assert
+        expect(policy.show()).toBe(false)
+      })
+    })
+
     describe("#create", () => {
       test("when the user has admin level access to the information sharing agreement, returns true", async () => {
         // Arrange
