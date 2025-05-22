0b614a4e90463a330ed6b0004cf65bd8285fd91f
caleb <calebburke91@gmail.com>
2025-05-22 13:45:31 -0700

    frontend notification model

---
+++ web/src/api/notifications-api.ts
@@ -0,0 +1,81 @@
+import http from "@/api/http-client"
+import { type Policy } from "@/api/base-api"
+
+// Keep in sync with api/src/models/notification.ts
+export enum NotificationSourceTypes {
+  SYSTEM = "system",
+}
+
+export type Notification = {
+  id: number
+  userId: number
+  isRead: boolean
+  readDate: string | null
+  title: string
+  subtitle: string | null
+  href: string | null
+  sourceType: NotificationSourceTypes
+  createdAt: string
+  updatedAt: string
+}
+
+export type NotificationWhereOptions = {
+  userId?: number
+  isRead?: boolean
+  sourceType?: NotificationSourceTypes
+}
+
+export type NotificationFiltersOptions = {
+  createdTodayInUserTimezone?: string
+}
+
+export type NotificationQueryOptions = {
+  where?: NotificationWhereOptions
+  filters?: NotificationFiltersOptions
+  page?: number
+  perPage?: number
+}
+
+export const notificationsApi = {
+  async list(params: NotificationQueryOptions = {}): Promise<{
+    notifications: Notification[]
+    totalCount: number
+  }> {
+    const { data } = await http.get("/api/notifications", {
+      params,
+    })
+    return data
+  },
+
+  async get(notificationId: number): Promise<{
+    notification: Notification
+    policy: Policy
+  }> {
+    const { data } = await http.get(`/api/notifications/${notificationId}`)
+    return data
+  },
+
+  async create(attributes: Partial<Notification>): Promise<{
+    notification: Notification
+  }> {
+    const { data } = await http.post("/api/notifications", attributes)
+    return data
+  },
+
+  async update(
+    notificationId: number,
+    attributes: Partial<Notification>
+  ): Promise<{
+    notification: Notification
+  }> {
+    const { data } = await http.patch(`/api/notifications/${notificationId}`, attributes)
+    return data
+  },
+
+  async delete(notificationId: number): Promise<void> {
+    const { data } = await http.delete(`/api/notifications/${notificationId}`)
+    return data
+  },
+}
+
+export default notificationsApi
---
+++ web/src/use/use-notifications.ts
@@ -0,0 +1,69 @@
+import { type Ref, reactive, toRefs, ref, unref, watch } from "vue"
+
+import notificationsApi, {
+  NotificationSourceTypes,
+  type Notification,
+  type NotificationQueryOptions,
+  type NotificationWhereOptions,
+  type NotificationFiltersOptions,
+} from "@/api/notifications-api"
+
+export {
+  NotificationSourceTypes,
+  type Notification,
+  type NotificationQueryOptions,
+  type NotificationWhereOptions,
+  type NotificationFiltersOptions,
+}
+
+export function useNotifications(
+  queryOptions: Ref<NotificationQueryOptions> = ref({}),
+  { skipWatchIf = () => false }: { skipWatchIf?: () => boolean } = {}
+) {
+  const state = reactive<{
+    notifications: Notification[]
+    totalCount: number
+    isLoading: boolean
+    isErrored: boolean
+  }>({
+    notifications: [],
+    totalCount: 0,
+    isLoading: false,
+    isErrored: false,
+  })
+
+  async function fetch(): Promise<Notification[]> {
+    state.isLoading = true
+    try {
+      const { notifications, totalCount } = await notificationsApi.list(unref(queryOptions))
+      state.isErrored = false
+      state.notifications = notifications
+      state.totalCount = totalCount
+      return notifications
+    } catch (error) {
+      console.error("Failed to fetch notifications:", error)
+      state.isErrored = true
+      throw error
+    } finally {
+      state.isLoading = false
+    }
+  }
+
+  watch(
+    () => [skipWatchIf(), unref(queryOptions)],
+    async ([skip]) => {
+      if (skip) return
+
+      await fetch()
+    },
+    { deep: true, immediate: true }
+  )
+
+  return {
+    ...toRefs(state),
+    fetch,
+    refresh: fetch,
+  }
+}
+
+export default useNotifications
