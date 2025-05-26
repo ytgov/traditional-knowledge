8d67ef967575aa57e08cd325d54733b8b55b516c
Marlen Brunner <klondikemarlen@gmail.com>
2025-05-26 16:27:52 -0700

    :recycle: Simplify ArchiveItemInformationSharingAgreementsPage component structure.

---
+++ web/src/components/information-sharing-agreement-archive-items/InformationSharingAgreementArchiveItemsAsInformationSharingAgreementsEditDataIterator.vue
@@ -0,0 +1,122 @@
+<template>
+  <v-data-iterator
+    v-model:items-per-page="perPage"
+    v-model:page="page"
+    :items="informationSharingAgreementArchiveItems"
+    :items-length="totalCount"
+    :loading="isLoading"
+    class="border rounded border-opacity-30 cursor-pointer"
+  >
+    <template #default="{ items }">
+      <v-list
+        class="py-0 border rounded border-opacity-30"
+        :loading="isLoading"
+      >
+        <InformationSharingAgreementListItem
+          v-for="({ raw: item }, index) in items"
+          :key="item.id"
+          :information-sharing-agreement-id="item.informationSharingAgreementId"
+          class="py-2"
+          :class="{
+            'border-b border-opacity-30': index < items.length - 1,
+          }"
+          @click.stop="goToInformationSharingAgreementPage(item.informationSharingAgreementId)"
+        >
+          <template #append>
+            <v-btn
+              :loading="isDeleting"
+              size="x-small"
+              icon="mdi-delete"
+              color="error"
+              @click.stop="deleteInformationSharingAgreementArchiveItem(item.id)"
+            />
+          </template>
+        </InformationSharingAgreementListItem>
+      </v-list>
+    </template>
+    <template #no-data>
+      <p class="text-center pa-3">No information sharing agreements for this item</p>
+    </template>
+  </v-data-iterator>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from "vue"
+import { useRouter } from "vue-router"
+import { useRouteQuery } from "@vueuse/router"
+
+import informationSharingAgreementArchiveItemsApi from "@/api/information-sharing-agreement-archive-items-api"
+import useInformationSharingAgreementArchiveItems, {
+  type InformationSharingAgreementArchiveItemWhereOptions,
+  type InformationSharingAgreementArchiveItemFiltersOptions,
+} from "@/use/use-information-sharing-agreement-archive-items"
+import useSnack from "@/use/use-snack"
+
+import InformationSharingAgreementListItem from "@/components/information-sharing-agreements/InformationSharingAgreementListItem.vue"
+
+const props = withDefaults(
+  defineProps<{
+    filters?: InformationSharingAgreementArchiveItemFiltersOptions
+    where?: InformationSharingAgreementArchiveItemWhereOptions
+    routeQuerySuffix?: string
+  }>(),
+  {
+    filters: () => ({}),
+    where: () => ({}),
+    routeQuerySuffix: "",
+  }
+)
+
+const page = useRouteQuery(`page${props.routeQuerySuffix}`, "1", { transform: Number })
+const perPage = useRouteQuery(`perPage${props.routeQuerySuffix}`, "10", { transform: Number })
+
+const informationSharingAgreementArchiveItemsQuery = computed(() => ({
+  filters: props.filters,
+  where: props.where,
+  perPage: perPage.value,
+  page: page.value,
+}))
+
+const { informationSharingAgreementArchiveItems, totalCount, isLoading, refresh } =
+  useInformationSharingAgreementArchiveItems(informationSharingAgreementArchiveItemsQuery)
+
+const isDeleting = ref(false)
+const snack = useSnack()
+
+async function deleteInformationSharingAgreementArchiveItem(
+  informationSharingAgreementArchiveItemId: number
+) {
+  isDeleting.value = true
+  try {
+    await informationSharingAgreementArchiveItemsApi.delete(
+      informationSharingAgreementArchiveItemId
+    )
+    snack.success("Stopped sharing with information sharing agreement.")
+    refresh()
+  } catch (error) {
+    console.error(`Failed to remove information sharing agreement archive item: ${error}`, {
+      error,
+    })
+    snack.error(`Failed to remove share to information sharing agreement: ${error}`)
+  } finally {
+    isDeleting.value = false
+  }
+}
+
+const router = useRouter()
+
+function goToInformationSharingAgreementPage(informationSharingAgreementId: number) {
+  return router.push({
+    name: "administration/information-sharing-agreements/InformationSharingAgreementPage",
+    params: {
+      informationSharingAgreementId,
+    },
+  })
+}
+
+defineExpose({
+  refresh,
+})
+</script>
+
+<style scoped></style>
--- web/src/pages/archive-items/ArchiveItemInformationSharingAgreementsPage.vue
+++ web/src/pages/archive-items/ArchiveItemInformationSharingAgreementsPage.vue
@@ -1,15 +1,75 @@
 <template>
-  <ArchiveItemAccessCard :archive-item-id="archiveItemIdAsNumber" />
+  <v-skeleton-loader
+    v-if="isNil(archiveItem)"
+    type="card"
+  />
+  <v-card v-else>
+    <template #text>
+      <div class="d-flex justify-end mb-4">
+        <v-btn
+          v-if="policy?.update"
+          color="primary"
+          variant="outlined"
+          @click="openGrantAccessDialog"
+        >
+          Share
+        </v-btn>
+      </div>
+      <div>
+        <InformationSharingAgreementArchiveItemsAsInformationSharingAgreementsEditDataIterator
+          ref="informationSharingAgreementArchiveItemsAsInformationSharingAgreementsEditDataIterator"
+          :where="informationSharingAgreementArchiveItemsWhereOptions"
+          route-query-suffix="InformationSharingAgreementArchiveItems"
+        />
+      </div>
+    </template>
+
+    <AddArchiveItemToInformationSharingAgreementDialog
+      ref="addArchiveItemToInformationSharingAgreementDialog"
+      @created="refreshAll"
+    />
+  </v-card>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue"
+import { computed, useTemplateRef } from "vue"
+import { isNil } from "lodash"
 
-import ArchiveItemAccessCard from "@/components/archive-item/ArchiveItemAccessCard.vue"
+import useArchiveItem from "@/use/use-archive-item"
+
+import AddArchiveItemToInformationSharingAgreementDialog from "@/components/information-sharing-agreement-archive-items/AddArchiveItemToInformationSharingAgreementDialog.vue"
+import InformationSharingAgreementArchiveItemsAsInformationSharingAgreementsEditDataIterator from "@/components/information-sharing-agreement-archive-items/InformationSharingAgreementArchiveItemsAsInformationSharingAgreementsEditDataIterator.vue"
 
 const props = defineProps<{
   archiveItemId: string
 }>()
 
 const archiveItemIdAsNumber = computed(() => parseInt(props.archiveItemId))
+const { archiveItem, policy, refresh } = useArchiveItem(archiveItemIdAsNumber)
+
+const informationSharingAgreementArchiveItemsWhereOptions = computed(() => ({
+  archiveItemId: archiveItemIdAsNumber.value,
+}))
+
+const addArchiveItemToInformationSharingAgreementDialog = useTemplateRef<
+  InstanceType<typeof AddArchiveItemToInformationSharingAgreementDialog>
+>("addArchiveItemToInformationSharingAgreementDialog")
+
+function openGrantAccessDialog() {
+  if (isNil(addArchiveItemToInformationSharingAgreementDialog.value)) return
+
+  addArchiveItemToInformationSharingAgreementDialog.value.show(archiveItemIdAsNumber.value)
+}
+
+const informationSharingAgreementArchiveItemsAsInformationSharingAgreementsEditDataIterator =
+  useTemplateRef<
+    InstanceType<
+      typeof InformationSharingAgreementArchiveItemsAsInformationSharingAgreementsEditDataIterator
+    >
+  >("informationSharingAgreementArchiveItemsAsInformationSharingAgreementsEditDataIterator")
+
+function refreshAll() {
+  refresh()
+  informationSharingAgreementArchiveItemsAsInformationSharingAgreementsEditDataIterator.value?.refresh()
+}
 </script>
