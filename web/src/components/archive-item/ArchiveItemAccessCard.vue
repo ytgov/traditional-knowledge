381d688ef21d20bb058a239a352459585c7cc9eb
Marlen Brunner <klondikemarlen@gmail.com>
2025-05-26 10:40:26 -0700

    :sparkles: Rework archive item access card to support information sharing agreement access grants

--- web/src/components/archive-item/ArchiveItemAccessCard.vue
+++ web/src/components/archive-item/ArchiveItemAccessCard.vue
@@ -4,24 +4,51 @@
     type="card"
   />
   <v-card v-else>
-    <v-card-title>Users with Access to this Item</v-card-title>
-    <v-card-text v-if="!isNil(archiveItem.permittedUsers) && archiveItem.permittedUsers.length > 0">
+    <template #title>Users with Access to this Item</template>
+    <template
+      v-if="
+        !isNil(archiveItem.informationSharingAgreementAccessGrants) &&
+        archiveItem.informationSharingAgreementAccessGrants.length > 0
+      "
+      #text
+    >
       <v-list
         class="py-0"
         style="border: 1px solid rgba(0, 0, 0, 0.3); border-radius: 4px"
       >
-        <v-list-item
-          v-for="(user, idx) of archiveItem.permittedUsers"
-          :key="user.id"
-          :title="`${user.displayName} : ${user.email}`"
-          :subtitle="`${user.department ?? 'Unknown department'} - ${user.title ?? 'Unknown title'}`"
-          class="py-2"
-          :class="{ 'border-bottom': idx < archiveItem.permittedUsers.length - 1 }"
+        <template
+          v-for="(
+            informationSharingAgreementAccessGrant, index
+          ) of archiveItem.informationSharingAgreementAccessGrants"
+          :key="informationSharingAgreementAccessGrant.userId"
         >
-        </v-list-item>
+          <UserListItem
+            v-if="!isNil(informationSharingAgreementAccessGrant.userId)"
+            :user-id="informationSharingAgreementAccessGrant.userId"
+            class="py-2"
+            :class="{
+              'border-bottom':
+                index < archiveItem.informationSharingAgreementAccessGrants.length - 1,
+            }"
+          />
+          <GroupListItem
+            v-else
+            :group-id="informationSharingAgreementAccessGrant.groupId"
+            class="py-2"
+            :class="{
+              'border-bottom':
+                index < archiveItem.informationSharingAgreementAccessGrants.length - 1,
+            }"
+          />
+        </template>
       </v-list>
-    </v-card-text>
-    <v-card-text v-else>No users have access to this item</v-card-text>
+    </template>
+    <template
+      v-else
+      #text
+    >
+      No users have access to this item
+    </template>
   </v-card>
 </template>
 
@@ -30,11 +57,13 @@ import { toRefs } from "vue"
 import { isNil } from "lodash"
 
 import useArchiveItem from "@/use/use-archive-item"
+import UserListItem from "@/components/users/UserListItem.vue"
 
 const props = defineProps<{
   archiveItemId: number
 }>()
 
+// TODO: switch to using a query to /information-sharing-agreement-access-grants
 const { archiveItemId } = toRefs(props)
 const { archiveItem } = useArchiveItem(archiveItemId)
 </script>
