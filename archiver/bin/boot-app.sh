9c9cc48492d974211952856d7faea6aad3925470
Marlen Brunner <klondikemarlen@gmail.com>
2025-06-24 16:37:00 -0700

    :sparkles: Add scheduler support to archiver service.

--- archiver/bin/boot-app.sh
+++ archiver/bin/boot-app.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ "$RUN_SCHEDULER" = "true" ]; then
+	echo "Running scheduler"
+	if [ "$NODE_ENV" = "production" ]; then
+		node ./dist/scheduler.js
+	else
+		npm run start:scheduler
+	fi
+	exit 0
+fi
+
 if [ "$NODE_ENV" = "production" ]; then
 	node ./dist/server.js
 else
