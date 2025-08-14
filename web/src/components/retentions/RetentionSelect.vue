c03f35f04076d05aa92145b7d560cb2a85b9f391
laurian_li <laurian.li@mail.utoronto.ca>
2025-08-14 12:55:09 -0700

    Copied from https://github.com/icefoganalytics/digital-vault/blob/main/web/src/components/

---
+++ web/src/components/retentions/RetentionSelect.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-select
+    :items="items"
+    item-value="id"
+    item-title="name"
+  />
+</template>
+
+<script setup lang="ts">
+import useRetentions from "@/use/use-retentions"
+import { onMounted } from "vue"
+
+const { items, list } = useRetentions()
+
+onMounted(async () => {
+  await list({ perPage: 1000, page: 1 })
+})
+</script>
