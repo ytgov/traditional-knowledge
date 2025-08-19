ee6b773f97f0f4a8492c7d2c630ff39e8dd330e5
laurian_li <laurian.li@mail.utoronto.ca>
2025-08-19 15:48:11 -0700

    Fixed migration and removing seeding

--- api/src/db/migrations/20250813204000_add-attribute-to-archive-items-table.ts
+++ api/src/db/migrations/20250813204000_add-attribute-to-archive-items-table.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable("archive_items", (table) => {
     table.text("sharing_purpose").notNullable().defaultTo("")
     table.boolean("confidentiality_receipt").notNullable().defaultTo(false)
-    table.string("yukon_first_nations", 255).notNullable().defaultTo("")
+    table.string("yukon_first_nations", 255).nullable()
   })
 }
 
