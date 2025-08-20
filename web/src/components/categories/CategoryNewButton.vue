17341782ef356b1c5a75b95a347d9613213ccabb
laurian_li <laurian.li@mail.utoronto.ca>
2025-08-20 10:49:07 -0700

    Added Retention and category operations

---
+++ web/src/components/categories/CategoryNewButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn
+    v-if="isSystemAdmin"
+    :to="{ name: 'administration/CategoryNewPage' }"
+    style="height: 40px"
+    >New Category</v-btn
+  >
+</template>
+
+<script setup lang="ts">
+import useCurrentUser from "@/use/use-current-user"
+
+const { isSystemAdmin } = useCurrentUser()
+</script>
---
+++ web/src/pages/administration/categories/CategoryNewPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <CategoryForm :category-id="categoryId" />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+import useBreadcrumbs, { ADMIN_CRUMB } from "@/use/use-breadcrumbs"
+import CategoryForm from "@/components/categories/CategoryForm.vue"
+
+const categoryId = ref(null)
+
+useBreadcrumbs("New Category", [
+  ADMIN_CRUMB,
+  {
+    title: "Categories",
+    to: {
+      name: "administration/CategoriesPage",
+    },
+  },
+])
+</script>
