0bb5751043ab2c53825a8987c6cdf8abadbfa6aa
Marlen Brunner <klondikemarlen@gmail.com>
2026-01-22 15:16:01 -0700

    :seedling: Ensure system user exists.

--- api/src/db/seeds/production/001.add-system-user.ts
+++ api/src/db/seeds/production/001.add-system-user.ts
@@ -25,10 +25,10 @@ export async function seed(_knex: Knex): Promise<void> {
     })
     if (isNil(user)) {
       user = await User.create(attributes)
-      logger.debug("User created:", user.dataValues)
+      logger.info("System user created:", user.dataValues)
     } else {
       await user.update(attributes)
-      logger.debug("User updated:", user.dataValues)
+      logger.info("System user updated:", user.dataValues)
     }
   }
 }
