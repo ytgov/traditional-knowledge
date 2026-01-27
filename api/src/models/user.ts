a0d65f601edfb77c78c0c786da0c6104d403a405
Marlen Brunner <klondikemarlen@gmail.com>
2026-01-27 11:25:31 -0700

    :sparkles: Add Yukon First Nations to field User model.

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -35,6 +35,24 @@ export enum UserRoles {
   USER = "user",
 }
 
+export enum UserYukonFirstNations {
+  CARCROSS_TAGISH_FIRST_NATION = "Carcross/Tagish First Nation",
+  CHAMPAGNE_AND_AISHIHIK_FIRST_NATIONS = "Champagne and Aishihik First Nations",
+  COUNCIL_OF_YUKON_FIRST_NATIONS = "Council of Yukon First Nations",
+  FIRST_NATION_OF_NACHO_NYAK_DUN = "First Nation of Na-Cho Nyäk Dun",
+  KLUANE_FIRST_NATION = "Kluane First Nation",
+  KWANLIN_DUN_FIRST_NATION = "Kwanlin Dün First Nation",
+  LIARD_FIRST_NATION = "Liard First Nation",
+  LITTLE_SALMON_CARMACKS_FIRST_NATION = "Little Salmon/Carmacks First Nation",
+  ROSS_RIVER_DENA_COUNCIL = "Ross River Dena Council",
+  SELKIRK_FIRST_NATION = "Selkirk First Nation",
+  TAAN_KWACHAN_COUNCIL = "Ta'an Kwäch'än Council",
+  TESLIN_TLINGIT_COUNCIL = "Teslin Tlingit Council",
+  TRONDEK_HWECHIN = "Tr'ondëk Hwëch'in",
+  VUNTUT_GWITCHIN_FIRST_NATION = "Vuntut Gwitchin First Nation",
+  WHITE_RIVER_FIRST_NATION = "White River First Nation",
+}
+
 export class User extends BaseModel<InferAttributes<User>, InferCreationAttributes<User>> {
   static readonly Roles = UserRoles
 
@@ -117,6 +135,15 @@ export class User extends BaseModel<InferAttributes<User>, InferCreationAttribut
 
   @Attribute(DataTypes.STRING(50))
   declare phoneNumber: string | null
+
+  @Attribute(DataTypes.STRING(100))
+  @ValidateAttribute({
+    isIn: {
+      args: [Object.values(UserYukonFirstNations)],
+      msg: `Yukon First Nation must be one of ${Object.values(UserYukonFirstNations).join(", ")}`,
+    },
+  })
+  declare yukonFirstNation: UserYukonFirstNations | null
 
   @Attribute(DataTypes.DATE(0))
   declare lastSyncSuccessAt: Date | null
