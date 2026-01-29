b29f0b4fbf763719f398b4b658970809140fb75d
Marlen Brunner <klondikemarlen@gmail.com>
2026-01-29 11:51:08 -0700

    :pencil: Improve text clarity by describing acronyms before first use.

--- web/src/locales/en.js
+++ web/src/locales/en.js
@@ -7,13 +7,13 @@ export default {
     },
     accessLevelDescriptions: {
       internal:
-        "INTERNAL to all YG employees across all departments. This TK is not accessible to the public and has been shared for YG's internal information only. Any YG employees who access this TK understand there could be negative impacts to intergovernmental relations if TK is made available to the public or unauthorised personnel.",
+        "INTERNAL to all YG employees across all departments. This Traditional Knowledge (TK) is not accessible to the public and has been shared for Yukon Government's (YG's) internal information only. Any YG employees who access this TK understand there could be negative impacts to intergovernmental relations if TK is made available to the public or unauthorised personnel.",
       protected_and_limited:
-        "PROTECTED and LIMITED to employees from {department}. TK is sensitive and could damage intergovernmental relations, negatively impact YG's reputation and/or legal position if TK is made available to the public or unauthorised personnel.",
+        "PROTECTED and LIMITED to employees from {department}. This Traditional Knowledge (TK) is sensitive and could damage intergovernmental relations, negatively impact Yukon Government's (YG's) reputation and/or legal position if TK is made available to the public or unauthorised personnel.",
       confidential_and_restricted:
-        "CONFIDENTIAL and RESTRICTED to employees within {departmentBranchUnitHeirarchy}. TK is highly sensitive or sacred and could significantly damage intergovernmental relations, reputation(s), result in legal action or risk harm if TK is made available to the public or unauthorised personnel.",
+        "CONFIDENTIAL and RESTRICTED to employees within {departmentBranchUnitHeirarchy}. This Traditional Knowledge (TK) is highly sensitive or sacred and could significantly damage intergovernmental relations, reputation(s), result in legal action or risk harm if TK is made available to the public or unauthorised personnel.",
       confidential_and_restricted_with_additional_restrictions:
-        "CONFIDENTIAL and RESTRICTED to employees within {departmentBranchUnitHeirarchy}, with {additionalAccessRestrictions}. TK is highly sensitive or sacred and could significantly damage intergovernmental relations, reputation(s), result in legal action or risk harm if TK is made available to the public or unauthorised personnel.",
+        "CONFIDENTIAL and RESTRICTED to employees within {departmentBranchUnitHeirarchy}, with {additionalAccessRestrictions}. This Traditional Knowledge (TK) is highly sensitive or sacred and could significantly damage intergovernmental relations, reputation(s), result in legal action or risk harm if TK is made available to the public or unauthorised personnel.",
     },
     confidentiality: {
       ACCORDANCE: "ACCORDANCE",
