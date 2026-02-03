d37afcb1b8eae63a04f71706649d0d071575999f
Marlen Brunner <klondikemarlen@gmail.com>
2026-02-03 13:28:33 -0700

    :construction: Basic standardization of information sharing agreement edit access page.
    Mainly move form inline.

--- web/src/pages/information-sharing-agreements/InformationSharingAgreementEditAccessPage.vue
+++ web/src/pages/information-sharing-agreements/InformationSharingAgreementEditAccessPage.vue
@@ -6,28 +6,121 @@
   <v-form
     v-else
     ref="form"
-    @submit.prevent="saveWrapper"
+    @submit.prevent="saveAndGoToNextPage"
   >
-    <v-row class="mt-2">
+    <p>
+      Authorised access to this Traditional Knowledge (TK) is limited. Any Yukon Government (YG)
+      employees who are authorised to consider this TK must have a work need that complies with the
+      criteria of this agreement, adhere to the terms and conditions, and uphold the equivalent
+      protection standards YG would normally apply to information provided by another government
+      that is considered:
+    </p>
+
+    <v-row class="mt-4">
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <InformationSharingAgreementAccessLevelSelect
+          v-model="informationSharingAgreement.accessLevel"
+          label="Access level *"
+          :rules="[required]"
+          required
+        />
+      </v-col>
+      <v-col
+        v-if="!isNil(informationSharingAgreement.accessLevel) && !isEmpty(description)"
+        cols="12"
+        md="6"
+      >
+        <v-card
+          class="rounded-lg"
+          variant="outlined"
+        >
+          <v-card-title class="d-flex align-center">
+            <v-icon size="x-small"> mdi-file-document </v-icon>
+            <h4 class="ml-2">Access Guidelines</h4>
+          </v-card-title>
+          <v-card-text class="font-italic">
+            {{ description }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row
+      v-if="
+        informationSharingAgreement.accessLevel ===
+        InformationSharingAgreementAccessLevels.PROTECTED_AND_LIMITED
+      "
+    >
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <v-text-field
+          v-model="informationSharingAgreement.accessLevelDepartmentRestriction"
+          label="Specify department *"
+          :rules="[required]"
+          required
+        />
+      </v-col>
+    </v-row>
+    <v-row
+      v-else-if="
+        informationSharingAgreement.accessLevel ===
+        InformationSharingAgreementAccessLevels.CONFIDENTIAL_AND_RESTRICTED
+      "
+    >
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <v-text-field
+          v-model="informationSharingAgreement.accessLevelDepartmentRestriction"
+          label="Specify department *"
+          :rules="[required]"
+          required
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <v-text-field
+          v-model="informationSharingAgreement.accessLevelBranchRestriction"
+          label="Specify branch *"
+          :rules="[required]"
+          required
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <v-text-field
+          v-model="informationSharingAgreement.accessLevelUnitRestriction"
+          label="Specify unit"
+        />
+      </v-col>
       <v-col cols="12">
-        <AccessLevelDescriptionCard
-          v-model:access-level="informationSharingAgreement.accessLevel"
-          v-model:access-level-department-restriction="
-            informationSharingAgreement.accessLevelDepartmentRestriction
-          "
-          v-model:access-level-branch-restriction="
-            informationSharingAgreement.accessLevelBranchRestriction
-          "
-          v-model:access-level-unit-restriction="
-            informationSharingAgreement.accessLevelUnitRestriction
-          "
-          v-model:has-additional-access-restrictions="
-            informationSharingAgreement.hasAdditionalAccessRestrictions
-          "
-          v-model:additional-access-restrictions="
-            informationSharingAgreement.additionalAccessRestrictions
-          "
-          class="border"
+        <v-switch
+          v-model="informationSharingAgreement.hasAdditionalAccessRestrictions"
+          label="Has ADDITIONAL MEASURES?"
+          hide-details
+        />
+      </v-col>
+      <v-col
+        v-if="informationSharingAgreement.hasAdditionalAccessRestrictions"
+        cols="12"
+      >
+        <v-textarea
+          v-model="informationSharingAgreement.additionalAccessRestrictions"
+          label="ADDITIONAL MEASURES *"
+          :rules="[required]"
+          required
+          auto-grow
+          rows="8"
         />
       </v-col>
     </v-row>
@@ -44,7 +137,10 @@
         </v-btn>
         <v-btn
           :to="{
-            name: 'administration/InformationSharingAgreementsPage',
+            name: 'information-sharing-agreements/InformationSharingAgreementEditDurationPage',
+            params: {
+              informationSharingAgreementId,
+            },
           }"
           class="mt-3 mt-md-0 ml-md-3"
           color="secondary"
@@ -61,13 +157,19 @@
 
 <script setup lang="ts">
 import { computed, useTemplateRef } from "vue"
+import { useRouter } from "vue-router"
 import { useDisplay } from "vuetify"
-import { isNil } from "lodash"
+import { useI18n } from "vue-i18n"
+import { isEmpty, isNil } from "lodash"
 
-import useInformationSharingAgreement from "@/use/use-information-sharing-agreement"
+import { required } from "@/utils/validators"
+
+import useInformationSharingAgreement, {
+  InformationSharingAgreementAccessLevels,
+} from "@/use/use-information-sharing-agreement"
 import useSnack from "@/use/use-snack"
 
-import AccessLevelDescriptionCard from "@/components/information-sharing-agreements/AccessLevelDescriptionCard.vue"
+import InformationSharingAgreementAccessLevelSelect from "@/components/information-sharing-agreements/InformationSharingAgreementAccessLevelSelect.vue"
 
 const props = defineProps<{
   informationSharingAgreementId: string
@@ -80,10 +182,71 @@ const { informationSharingAgreement, isLoading, save } = useInformationSharingAg
   informationSharingAgreementIdAsNumber
 )
 
+const { t } = useI18n()
+
+const description = computed(() => {
+  if (isNil(informationSharingAgreement.value)) return ""
+
+  const { accessLevel } = informationSharingAgreement.value
+  if (isNil(accessLevel)) return ""
+
+  let description = ""
+  if (accessLevel === InformationSharingAgreementAccessLevels.INTERNAL) {
+    description = t(`informationSharingAgreement.accessLevelDescriptions.${accessLevel}`)
+  }
+
+  const {
+    accessLevelDepartmentRestriction,
+    accessLevelBranchRestriction,
+    accessLevelUnitRestriction,
+    hasAdditionalAccessRestrictions,
+    additionalAccessRestrictions,
+  } = informationSharingAgreement.value
+
+  if (
+    accessLevel === InformationSharingAgreementAccessLevels.PROTECTED_AND_LIMITED &&
+    accessLevelDepartmentRestriction
+  ) {
+    description = t(`informationSharingAgreement.accessLevelDescriptions.${accessLevel}`, {
+      department: accessLevelDepartmentRestriction,
+    })
+  } else if (
+    accessLevel === InformationSharingAgreementAccessLevels.CONFIDENTIAL_AND_RESTRICTED &&
+    accessLevelBranchRestriction
+  ) {
+    const departmentBranchUnitHeirarchy = [
+      accessLevelDepartmentRestriction,
+      accessLevelBranchRestriction,
+      accessLevelUnitRestriction,
+    ]
+      .filter(Boolean)
+      .join(" / ")
+    if (!hasAdditionalAccessRestrictions) {
+      description = t(
+        "informationSharingAgreement.accessLevelDescriptions.confidential_and_restricted",
+        {
+          departmentBranchUnitHeirarchy,
+        }
+      )
+    } else if (hasAdditionalAccessRestrictions && !isEmpty(additionalAccessRestrictions)) {
+      description = t(
+        "informationSharingAgreement.accessLevelDescriptions.confidential_and_restricted_with_additional_restrictions",
+        {
+          departmentBranchUnitHeirarchy,
+          additionalAccessRestrictions,
+        }
+      )
+    }
+  }
+
+  return description
+})
+
+const router = useRouter()
 const form = useTemplateRef("form")
 const snack = useSnack()
 
-async function saveWrapper() {
+async function saveAndGoToNextPage() {
   if (isNil(form.value)) return
 
   const { valid } = await form.value.validate()
@@ -95,6 +258,13 @@ async function saveWrapper() {
   try {
     await save()
     snack.success("Access Level updated.")
+
+    await router.push({
+      name: "information-sharing-agreements/InformationSharingAgreementEditConfidentialityPage",
+      params: {
+        informationSharingAgreementId: props.informationSharingAgreementId,
+      },
+    })
   } catch (error) {
     console.error(`Failed to update access level: ${error}`, { error })
     snack.error(`Failed to update access level: ${error}`)
