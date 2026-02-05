42ab46ea058461ece31f1b39388df8991a85d4fe
Marlen Brunner <klondikemarlen@gmail.com>
2026-02-05 12:29:59 -0700

    :japanese_castle: Limit title length to 80 characters in acknowledgement template.

--- api/src/serializers/information-sharing-agreements/generate-acknowledgement/create-serializer.ts
+++ api/src/serializers/information-sharing-agreements/generate-acknowledgement/create-serializer.ts
@@ -1,21 +1,30 @@
+import { truncate } from "lodash"
+
 import { InformationSharingAgreement } from "@/models"
 import BaseSerializer from "@/serializers/base-serializer"
 
-export type InformationSharingAgreementAsAcknowledgement = Pick<
-  InformationSharingAgreement,
-  "title"
-> & {
+const IDENTIFIER_MAX_LENGTH = 80
+
+export type InformationSharingAgreementAsAcknowledgement = {
   identifier: string
 }
 
 export class CreateSerializer extends BaseSerializer<InformationSharingAgreement> {
   perform(): InformationSharingAgreementAsAcknowledgement {
     const { id, title } = this.record
-    const identifier = `${title} - ISA#${id}`
+    const identifier = this.buildIdentifier(id, title)
     return {
-      title,
       identifier,
     }
+  }
+
+  private buildIdentifier(id: number, title: string): string {
+    const suffix = ` - ISA#${id}`
+    const titleMaxLength = IDENTIFIER_MAX_LENGTH - suffix.length
+    const truncatedTitle = truncate(title, {
+      length: titleMaxLength,
+    })
+    return `${truncatedTitle}${suffix}`
   }
 }
 
