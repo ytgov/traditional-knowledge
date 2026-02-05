930c7a40b9021cd71e246586ec9c585b12d212b2
Marlen Brunner <klondikemarlen@gmail.com>
2026-02-05 12:14:28 -0700

    :bug: Fix sidebar navigation effect on medium screens.
    Previously was switching to "overlay" mode on medium screens.

--- web/src/components/default-layout/LeftSidebarNavigationDrawer.vue
+++ web/src/components/default-layout/LeftSidebarNavigationDrawer.vue
@@ -1,7 +1,6 @@
 <template>
   <v-navigation-drawer
     v-model="showDrawer"
-    :disable-resize-watcher="false"
     :rail="showRail"
     mobile-breakpoint="md"
     color="#a3a48d"
@@ -54,17 +53,28 @@
 
 <script setup lang="ts">
 import { ref } from "vue"
+import { useDisplay } from "vuetify"
 
 import useCurrentUser from "@/use/use-current-user"
 
-const showDrawer = defineModel<boolean>({
-  default: false,
-})
-const showRail = defineModel<boolean>("showRail", {
-  default: false,
-})
+const { mdAndUp } = useDisplay()
+
+const showDrawer = ref(mdAndUp.value)
+const showRail = ref(false)
+
+function toggle() {
+  if (!mdAndUp.value) {
+    showDrawer.value = !showDrawer.value
+  } else {
+    showRail.value = !showRail.value
+  }
+}
 
 const open = ref([])
 
 const { isAdmin } = useCurrentUser<true>()
+
+defineExpose({
+  toggle,
+})
 </script>
