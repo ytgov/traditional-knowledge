dfc796d5d3fa2b4d18d6d4516ce0203c9f1127ec
Marlen Brunner <klondikemarlen@gmail.com>
2026-02-06 14:07:49 -0700

    :fire: Remove legacy document generation service.

--- api/src/services/information-sharing-agreements/index.ts
+++ api/src/services/information-sharing-agreements/index.ts
@@ -1,7 +1,5 @@
 export { CreateService } from "./create-service"
 export { UpdateService } from "./update-service"
-export { DocumentGeneratorService } from "./document-generator-service"
-
 // Special services
 export { EnsureAdminAccessService } from "./admin-access-grants-service"
 
