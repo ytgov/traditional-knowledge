5ce45b2fe35a76420121689703c7a29032284db0
Marlen Brunner <klondikemarlen@gmail.com>
2026-02-27 08:58:28 -0700

    :bug: Fix handling of page level params.
    Page level params will always be "string", but and then should be converted to "number".

--- web/src/pages/information-sharing-agreements/archive-items/InformationSharingAgreementArchiveItemNewPage.vue
+++ web/src/pages/information-sharing-agreements/archive-items/InformationSharingAgreementArchiveItemNewPage.vue
@@ -1,15 +1,21 @@
 <template>
-  <InformationSharingAgreementCreateArchiveItemForm :information-sharing-agreement-id="informationSharingAgreementId" />
+  <InformationSharingAgreementCreateArchiveItemForm
+    :information-sharing-agreement-id="informationSharingAgreementIdAsNumber"
+  />
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue"
+
 import useBreadcrumbs, { BASE_CRUMB } from "@/use/use-breadcrumbs"
 
 import InformationSharingAgreementCreateArchiveItemForm from "@/components/information-sharing-agreements/archive-items/InformationSharingAgreementArchiveItemCreateForm.vue"
 
 const props = defineProps<{
-  informationSharingAgreementId: number
+  informationSharingAgreementId: string
 }>()
+
+const informationSharingAgreementIdAsNumber = computed(() => parseInt(props.informationSharingAgreementId))
 
 useBreadcrumbs("Create Knowledge Item from Agreement", [
   BASE_CRUMB,
