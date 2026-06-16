f7a8aee679129dab970da818d53fff508124967c
Caleb Burke <calebburke91@gmail.com>
2026-06-16 13:50:33 -0700

    Simplifying ISA archive item create steps

--- web/src/pages/information-sharing-agreements/archive-items/InformationSharingAgreementArchiveItemNewPage.vue
+++ web/src/pages/information-sharing-agreements/archive-items/InformationSharingAgreementArchiveItemNewPage.vue
@@ -1,38 +1,78 @@
 <template>
-  <InformationSharingAgreementCreateArchiveItemForm
-    :information-sharing-agreement-id="informationSharingAgreementIdAsNumber"
+  <v-skeleton-loader
+    v-if="isLoading || isNil(informationSharingAgreement)"
+    type="card"
   />
+  <div v-else>
+    <InformationSharingAgreementBasicInformationCard
+      :title="informationSharingAgreement.title"
+      :purpose="informationSharingAgreement.purpose"
+      :external-group-contact-id="informationSharingAgreement.externalGroupContactId"
+      :external-group-contact-title="informationSharingAgreement.externalGroupContactTitle"
+      :internal-group-contact-id="informationSharingAgreement.internalGroupContactId"
+      :internal-group-contact-title="informationSharingAgreement.internalGroupContactTitle"
+      :internal-group-secondary-contact-id="
+        informationSharingAgreement.internalGroupSecondaryContactId
+      "
+    >
+      <template #title>
+        <v-icon
+          color="accent"
+          icon="mdi-information-outline"
+        />
+        ISA Basic Information
+      </template>
+    </InformationSharingAgreementBasicInformationCard>
+
+    <InformationSharingAgreementArchiveItemCreateForm
+      class="mt-4"
+      :information-sharing-agreement="informationSharingAgreement"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { isNil } from "lodash"
 import { computed } from "vue"
 
 import useBreadcrumbs, { BASE_CRUMB } from "@/use/use-breadcrumbs"
+import useInformationSharingAgreement from "@/use/use-information-sharing-agreement"
 
-import InformationSharingAgreementCreateArchiveItemForm from "@/components/information-sharing-agreements/archive-items/InformationSharingAgreementArchiveItemCreateForm.vue"
+import InformationSharingAgreementBasicInformationCard from "@/components/information-sharing-agreements/InformationSharingAgreementBasicInformationCard.vue"
+import InformationSharingAgreementArchiveItemCreateForm from "@/components/information-sharing-agreements/archive-items/InformationSharingAgreementArchiveItemCreateForm.vue"
 
 const props = defineProps<{
   informationSharingAgreementId: string
 }>()
 
-const informationSharingAgreementIdAsNumber = computed(() => parseInt(props.informationSharingAgreementId))
+const informationSharingAgreementIdAsNumber = computed(() =>
+  parseInt(props.informationSharingAgreementId)
+)
 
-useBreadcrumbs("Create Knowledge Item from Agreement", [
-  BASE_CRUMB,
-  {
-    title: "Information Sharing Agreements",
-    to: {
-      name: "InformationSharingAgreementsPage",
-    },
-  },
-  {
-    title: `Agreement ${props.informationSharingAgreementId}`,
-    to: {
-      name: "information-sharing-agreements/InformationSharingAgreementPage",
-      params: {
-        informationSharingAgreementId: props.informationSharingAgreementId,
+const { informationSharingAgreement, isLoading } = useInformationSharingAgreement(
+  informationSharingAgreementIdAsNumber
+)
+
+const breadcrumbs = computed(() => {
+  return [
+    BASE_CRUMB,
+    {
+      title: "Information Sharing Agreements",
+      to: {
+        name: "InformationSharingAgreementsPage",
       },
     },
-  },
-])
+    {
+      title: informationSharingAgreement.value?.title ?? "Loading...",
+      to: {
+        name: "information-sharing-agreements/InformationSharingAgreementPage",
+        params: {
+          informationSharingAgreementId: props.informationSharingAgreementId,
+        },
+      },
+    },
+  ]
+})
+
+useBreadcrumbs("Create Knowledge Item from Agreement", breadcrumbs)
 </script>
