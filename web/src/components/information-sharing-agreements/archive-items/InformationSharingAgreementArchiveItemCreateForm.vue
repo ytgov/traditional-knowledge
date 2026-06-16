b2c44f6c4577eb40a64a8bff6293c5c1f883cf1d
Caleb Burke <calebburke91@gmail.com>
2026-06-16 13:50:33 -0700

    Simplifying ISA archive item create steps

--- web/src/components/information-sharing-agreements/archive-items/InformationSharingAgreementArchiveItemCreateForm.vue
+++ web/src/components/information-sharing-agreements/archive-items/InformationSharingAgreementArchiveItemCreateForm.vue
@@ -6,90 +6,7 @@
   >
     <v-card class="border">
       <FileDrop @files-dropped="attachDroppedFiles">
-        <v-card-title>Knowledge Item Description</v-card-title>
-        <v-card-text>
-          <v-row>
-            <v-col
-              cols="12"
-              md="8"
-            >
-              <v-text-field
-                v-model="archiveItemAttributes.title"
-                label="Title *"
-                :rules="[required]"
-                required
-              />
-            </v-col>
-            <v-col
-              cols="12"
-              md="4"
-            >
-              <SecurityLevelSelect
-                v-model="archiveItemAttributes.securityLevel"
-                :rules="[required]"
-                label="Security level *"
-                required
-              />
-            </v-col>
-            <v-col cols="12">
-              <v-textarea
-                v-model="archiveItemAttributes.description"
-                label="Description *"
-                :rules="[required]"
-                auto-grow
-                required
-                rows="8"
-              />
-            </v-col>
-          </v-row>
-        </v-card-text>
-
-        <v-card-title>Yukon First Nation</v-card-title>
-        <v-card-text>
-          <p class="mb-4">
-            The Yukon First Nation is determined by the sharing contact on the Information Sharing
-            Agreement.
-          </p>
-          <ExternalOrganizationChip
-            v-if="externalGroupContactOrganization"
-            :external-organization-id="externalGroupContactOrganization.id"
-            variant="outlined"
-          />
-          <span
-            v-else
-            class="text-medium-emphasis"
-          >
-            No organization found for the sharing contact on this agreement.
-          </span>
-        </v-card-text>
-
-        <v-card-title>Sharing Purpose</v-card-title>
-        <v-card-text>
-          <p class="mb-3">For what purpose this traditional knowledge is shared?</p>
-          <v-textarea
-            v-model="archiveItemAttributes.sharingPurpose"
-            label="Sharing Purpose *"
-            :rules="[required]"
-            auto-grow
-            required
-            rows="8"
-          />
-          <v-checkbox
-            v-model="archiveItemAttributes.confidentialityReceipt"
-            label="I confirm that I have received and agreed to the confidentiality terms."
-            class="mt-3"
-            :rules="[required]"
-            required
-            hide-details
-          />
-        </v-card-text>
-
-        <v-divider
-          thickness="2"
-          class="mb-4"
-        />
-
-        <v-card-title>Categories and Tags</v-card-title>
+        <v-card-title> Categories and Tags </v-card-title>
         <v-card-text>
           <p class="mb-4">
             Categories and Tags are used as filter criteria to find items. You can select as many of
@@ -130,8 +47,8 @@
             type="submit"
             class="mt-5"
             :loading="isLoading"
-            >Save</v-btn
-          >
+            text="Create"
+          />
         </v-card-text>
       </FileDrop>
     </v-card>
@@ -139,7 +56,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, toRefs, useTemplateRef, watchEffect } from "vue"
+import { computed, ref, useTemplateRef, watchEffect } from "vue"
 import { useRouter } from "vue-router"
 import { isNil } from "lodash"
 
@@ -148,28 +65,22 @@ import { required } from "@/utils/validators"
 import Api from "@/api"
 import { type ArchiveItemCreationAttributes } from "@/api/information-sharing-agreements/archive-items-api"
 import { SecurityLevel } from "@/api/archive-items-api"
-import { InformationSharingAgreementAccessLevels } from "@/api/information-sharing-agreements-api"
+import {
+  InformationSharingAgreementAccessLevels,
+  type InformationSharingAgreementAsShow,
+} from "@/api/information-sharing-agreements-api"
 
 import useExternalOrganizations from "@/use/use-external-organizations"
-import useInformationSharingAgreement from "@/use/use-information-sharing-agreement"
 import useSnack from "@/use/use-snack"
 
 import EnhancedFileInput from "@/components/common/EnhancedFileInput.vue"
 import FileDrop from "@/components/common/FileDrop.vue"
 
 import CategorySelect from "@/components/categories/CategorySelect.vue"
-import ExternalOrganizationChip from "@/components/external-organizations/ExternalOrganizationChip.vue"
-
-import SecurityLevelSelect from "@/components/archive-items/SecurityLevelSelect.vue"
 
 const props = defineProps<{
-  informationSharingAgreementId: number
+  informationSharingAgreement: InformationSharingAgreementAsShow
 }>()
-
-const { informationSharingAgreementId } = toRefs(props)
-const { informationSharingAgreement } = useInformationSharingAgreement(
-  informationSharingAgreementId
-)
 
 const archiveItemAttributes = ref<Partial<ArchiveItemCreationAttributes>>({
   title: "",
@@ -189,21 +100,19 @@ const ACCESS_LEVEL_TO_SECURITY_LEVEL = {
 }
 
 watchEffect(() => {
-  if (informationSharingAgreement.value) {
-    const { accessLevel, purpose, authorizedApplication, title } = informationSharingAgreement.value
+  const { accessLevel, purpose, authorizedApplication, title } = props.informationSharingAgreement
 
-    const accessLevelOrDefault = accessLevel ?? InformationSharingAgreementAccessLevels.INTERNAL
+  const accessLevelOrDefault = accessLevel ?? InformationSharingAgreementAccessLevels.INTERNAL
 
-    archiveItemAttributes.value.securityLevel = ACCESS_LEVEL_TO_SECURITY_LEVEL[accessLevelOrDefault]
-    archiveItemAttributes.value.sharingPurpose = authorizedApplication
-    archiveItemAttributes.value.description = purpose
-    archiveItemAttributes.value.title = title
-  }
+  archiveItemAttributes.value.securityLevel = ACCESS_LEVEL_TO_SECURITY_LEVEL[accessLevelOrDefault]
+  archiveItemAttributes.value.sharingPurpose = authorizedApplication
+  archiveItemAttributes.value.description = purpose
+  archiveItemAttributes.value.title = title
 })
 
 const NULL_USER_ID = -1
 const externalGroupContactId = computed(
-  () => informationSharingAgreement.value?.externalGroupContactId
+  () => props.informationSharingAgreement.externalGroupContactId
 )
 const externalOrganizationsQuery = computed(() => ({
   filters: {
@@ -254,7 +163,7 @@ async function saveAndRedirect() {
   isLoading.value = true
   try {
     const { archiveItem } = await Api.InformationSharingAgreements.archiveItemsApi.create(
-      props.informationSharingAgreementId,
+      props.informationSharingAgreement.id,
       archiveItemAttributes.value,
       files.value
     )
